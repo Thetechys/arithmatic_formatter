114d868510fe80187004a31340f69da4207a038b
Liau <yong.siang.liau1@hp.com>
2023-03-04 04:14:16 +0800

    major changes, including removed <split_data > which split strings using \+\-, replaced it with <split_problems> which split strings using split method without argument/s

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -2,9 +2,10 @@ import re
 
 
 
-def arithmetic_arranger(problems,*args):
+def arithmetic_arranger(problems,show=False):
 
     strip_problem = [q.replace(' ','') for q in problems]
+    split_problem = [s.split() for s in problems]
     pattern = re.compile("[+\-]")
     l_operand = list()
     operator = list()
@@ -17,58 +18,63 @@ def arithmetic_arranger(problems,*args):
     green_light = 0
 
 
-    for i in strip_problem:   ### for loop that trigger error msg if argument doesn't meet requirement
 
-        if re.search('\+|\-',i) == None: 
-            
-            return "Error: Operator must be '+' or '-'"
-            
-            
-        elif re.match('[\d]{1,4}[\+|\-][\d]{1,4}',i) == None: 
-            
-            return"Error: Numbers cannot be more than four digits."
-            
+    for i in split_problem:   ### for loop that trigger error msg if argument doesn't meet requirement
 
-        elif len(strip_problem) > 4: 
+
+        if re.search('\+|\-',i[1]) == None: 
             
-            return "Error: Too many problems"
+            return print("Error: Operator must be '+' or '-'.")
+            
+            
+        elif (len(i[0]) > 4) or (len(i[2]) > 4) : 
+            
+            return print("Error: Numbers cannot be more than four digits.")
             
 
-        elif re.match('[\d]+[\+|\-][\d]+',strip_problem[strip_problem.index(i)]) == None:
+        elif len(strip_problem) > 5: 
             
-            return '#Error: Numbers must only contain digits.'
+            return print("Error: Too many problems.")
+            
+
+        elif (i[0].isnumeric() and i[2].isnumeric()) == False:
+            
+            return print("Error: Numbers must only contain digits.")
         
 
         else:
-            #print('checked no problem, proceed format arrangement')  ## debug purposes
             green_light = 1
+
 
 
     if green_light == 1:
 
+
         
-        for i in strip_problem:
+        for i in split_problem:
             
-            opfind = re.search('[+\-]',i)
-            split_data = re.split(pattern,i)
-            pad_len = max(len(split_data[0]),len(split_data[1]))
+            pad_len = max(len(i[0]),len(i[2]))
             padding.append(int(pad_len))
 
+            print(f'pad len = {pad_len}')
+            print(padding)
 
-            operator.append(i[opfind.start()])   ## appending '+' and '-' into list
-            l_operand.append(split_data[0])  ## appending left operand in string type
-            num_l_operand.append(int(split_data[0])) ## convern and append left operant in int type
-            r_operand.append(split_data[1])  ## right operand in string type
-            num_r_operand.append(int(split_data[1]))  ## right operand in int type
 
-            if re.search('\+',i) != None:
+            operator.append(i[1])   ## appending '+' and '-' into list
+            l_operand.append(i[0])  ## appending left operand in string type
+            num_l_operand.append(int(i[0])) ## convern and append left operant in int type
+            r_operand.append(i[2])  ## right operand in string type
+            num_r_operand.append(int(i[2]))  ## right operand in int type
 
-                ans = int(split_data[0]).__add__(int(split_data[1]))
+
+            if re.search('\+',i[1]) != None:
+
+                ans = int(i[0]).__add__(int(i[2]))
                 num_answer.append(ans)
 
             else:
                 
-                ans = int(split_data[0]).__sub__(int(split_data[1]))
+                ans = int(i[0]).__sub__(int(i[2]))
                 num_answer.append(ans)
 
 
@@ -77,15 +83,42 @@ def arithmetic_arranger(problems,*args):
             breakline.append(''.rjust(padding[i]+2,'-')) #added 2 , one is for space btwn operator and r_operand , another is right under opertor
 
 
-        l_concate = '    '.join([i.rjust(padding[l_operand.index(i)]+2) for i in l_operand])
+        # l_concate = '    '.join([i.rjust(padding[l_operand.index(i)]+2) for i in l_operand])
+        # r_o_concate = '    '.join([operator[i] + r_operand[i].rjust(padding[i]+1) for i in range(len(operator))])
+        # ans_concate = '    '.join([str(num_answer[i]).rjust(len(breakline[i])) for i in range(len(num_answer))])
+
+
+
+
+        l_concate = '    '.join([i.rjust(padding[index]+2) for index, i in enumerate(l_operand)])
         r_o_concate = '    '.join([operator[i] + r_operand[i].rjust(padding[i]+1) for i in range(len(operator))])
         ans_concate = '    '.join([str(num_answer[i]).rjust(len(breakline[i])) for i in range(len(num_answer))])
 
         breakline = '    '.join(breakline)
 
+        print(l_operand) ## debug
+        print(r_operand)  ## debug
 
-        arranged_problems = l_concate+'\n'+r_o_concate+'\n'+breakline+'\n'+ans_concate 
 
-        if args == True:
+        if show == True:
 
-            return arranged_problems
+            arranged_problems = print(l_concate+'\n'+r_o_concate+'\n'+breakline+'\n'+ans_concate)
+
+        else:
+
+            arranged_problems = print(l_concate+'\n'+r_o_concate+'\n'+breakline)
+
+
+        return arranged_problems
+    
+
+
+arithmetic_arranger(['11 + 4', '3801 - 2999', '1 + 2', '123 + 49', '1 - 9380'])
+
+## ['1 + 2', '1 - 9380']  the 2nd '1' won't align due to python though it is the 1st '1'    SOLVED
+
+## ['11 + 4', '3801 - 2999', '1 + 2', '123 + 49', '1 - 9380']   same issue as above    SOLVED
+
+## ['24 + 85215', '3801 - 2', '45 + 43', '123 + 49'] doesnt trigge error "Error: Numbers cannot be more than four digits." SOLVED
+
+## ['98 + 3g5', '3801 - 2', '45 + 43', '123 + 49'] this test triggered a python value error instead of "Error: Numbers must only contain digits."   SOLVED
